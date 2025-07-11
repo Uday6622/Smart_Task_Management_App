5986438a79490b3b7c8b3e61ecc41a5ea8a03127
Uday <167074135+Uday6622@users.noreply.github.com>
2025-07-11 09:44:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -359,4 +359,4 @@ if __name__ == "__main__":
             db.session.commit()
             print("✅ Default admin created: admin@example.com / admin123")
 
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
